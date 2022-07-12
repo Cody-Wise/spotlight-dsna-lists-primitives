e147c9cf43830bbaff5821d8ba97641fbec2f11d
Elliott Cheifetz <elliottsmyname@gmail.com>
2022-07-12 10:41:54 -0700

    small change

--- index.test.js
+++ index.test.js
@@ -4,7 +4,7 @@ describe("reverseWords", () => {
   it("should reverse the words of a sentence", () => {
     expect(reverseWords("alchemy rocks gold")).toBe("ymehcla skcor dlog");
   });
-  it.only("should cap first letter of each word", () => {
+  it.only("should capitalize first letter of each word", () => {
     expect(capitalizeWords("alchemy rocks gold")).toBe("Alchemy Rocks Gold");
   });
 });
