d3214764879b500e72ebfc7d2b9e53f627e184bf
Elliott Cheifetz <elliottsmyname@gmail.com>
2022-07-12 10:40:36 -0700

    CH2

--- index.js
+++ index.js
@@ -7,4 +7,13 @@ function reverseWords(sentence) {
   return reversedSentence;
 }
 
-module.exports = { reverseWords };
+function capitalizeWords(sentence) {
+  const capitalizedSentence = sentence
+     .split(' ')
+     .map((elem) => elem.charAt(0).toUpperCase() + elem.substring(1).toLowerCase())
+     .join(' ');
+
+  return capitalizedSentence;
+}
+
+module.exports = { reverseWords, capitalizeWords };
--- index.test.js
+++ index.test.js
@@ -1,7 +1,10 @@
-const { reverseWords } = require(".");
+const { reverseWords, capitalizeWords } = require(".");
 
 describe("reverseWords", () => {
   it("should reverse the words of a sentence", () => {
     expect(reverseWords("alchemy rocks gold")).toBe("ymehcla skcor dlog");
+  });
+  it.only("should cap first letter of each word", () => {
+    expect(capitalizeWords("alchemy rocks gold")).toBe("Alchemy Rocks Gold");
   });
 });
